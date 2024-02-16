7a67840579c0993b40111b852f8f79cd8b61eb01
Aramdhann <radityamochamad@gmail.com>
2024-02-16 11:34:52 +0700

    implement ant design and testing to integrate it

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Input } from "antd";
 
 const Login = () => {
   return (
-    <div>
-      <div>Login</div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="border rounded-lg p-2 flex gap-3 w-[700px]">
+        <div className="flex border rounded-lg w-[200px] h-[200px]"></div>
+        <div className="border rounded-lg p-5 w-full">
+          <p className="font-bold">LOGIN</p>
+          <Input placeholder="email@gmail.com" />
+        </div>
+      </div>
     </div>
   );
 };
