f064a3f5cd367d13a0693bf309d0ba268b1ff56a
Aramdhann <radityamochamad@gmail.com>
2024-02-16 11:25:33 +0700

    fix basic file and adding tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { Lazy } from "react";
+import React, { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/Login";
-import Home from "./pages/Home.jsx";
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
 
 function App() {
   return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
+import React from "react";
+
 const Login = () => {
   return (
     <div>
-      <p>Login</p>
+      <div>Login</div>
     </div>
   );
 };
